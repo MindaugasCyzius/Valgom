1cb568d9abe15ea3bd1cd0e2803ff97bb89cd74d
raluzez <lukaslaurina@gmail.com>
2019-07-14 16:36:13 +0300

    bootstrap

--- valgom/src/App.js
+++ valgom/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.scss';
+import {Button} from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
       <p>React app</p>
+      <Button success></Button>
       <input type="text"/>
       <input type="date"/>
     </div>
