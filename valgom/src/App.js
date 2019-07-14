291f685f2f23107f273ae21cac6bcb4f963bf5c1
Mindaugas Čyžius <mindaugas.cyzius@gmail.com>
2019-07-14 16:45:28 +0300

    Component

--- valgom/src/App.js
+++ valgom/src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.scss';
 import {Button} from 'react-bootstrap'
 
-function App() {
-  return (
-    <div className="App">
-      <p>React app</p>
-      <Button success></Button>
-      <input type="text"/>
-      <input type="date"/>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return(
+      <div className="App">
+        <p>React app</p>
+        <Button success></Button>
+        <input type="text"/>
+        <input type="date"/>
+      </div>
+    );
+  } 
 }
 
 export default App;
